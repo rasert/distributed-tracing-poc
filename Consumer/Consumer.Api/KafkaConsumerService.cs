503fa0dc1154d6c0110138a668a0c215b1c8a973
J4XN_ <jackson.mourao@grupoboticario.com.br>
2024-07-12 16:34:39 -0300

    Created .net consumer app.

---
+++ Consumer/Consumer.Api/KafkaConsumerService.cs
@@ -0,0 +1,60 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Extensions.Diagnostics;
+
+namespace Consumer.Api
+{
+    public class KafkaConsumerService : BackgroundService
+    {
+        private readonly IConsumer<Ignore, string> _consumer;
+        private readonly ILogger<KafkaConsumerService> _logger;
+
+        public KafkaConsumerService(IConfiguration configuration, ILogger<KafkaConsumerService> logger)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = configuration["Kafka:BootstrapServers"],
+                GroupId = "consumer-group",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            _consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+            _consumer.Subscribe("text-topic");
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    await _consumer.ConsumeWithInstrumentation((result, stoppingToken) =>
+                    {
+                        if (!stoppingToken.IsCancellationRequested && result != null)
+                        {
+                            string text = result?.Message.Value ?? string.Empty;
+                            _logger.LogInformation($"Consumed message '{text}' at: '{result?.TopicPartitionOffset}'.");
+
+                            // TODO: Send HTTP request to Golang App
+
+                        }
+                        return Task.CompletedTask;
+                    }, stoppingToken);
+                }
+                catch (ConsumeException e)
+                {
+                    _logger.LogError($"Error occured: {e.Error.Reason}");
+                }
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+
+        public override void Dispose()
+        {
+            _consumer.Close();
+            _consumer.Dispose();
+            base.Dispose();
+        }
+    }
+}
---
+++ Consumer/Consumer.Api/Program.cs
@@ -0,0 +1,71 @@
+using Confluent.Kafka.Extensions.OpenTelemetry;
+using Consumer.Api;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+const string serviceName = "consumer";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(serviceName))
+        .AddOtlpExporter();
+});
+builder.Services.AddOpenTelemetry()
+      .ConfigureResource(resource => resource.AddService(serviceName))
+      .WithTracing(tracing => tracing
+          .AddAspNetCoreInstrumentation()
+          .AddConfluentKafkaInstrumentation()
+          .AddOtlpExporter())
+      .WithMetrics(metrics => metrics
+          .AddAspNetCoreInstrumentation()
+          .AddOtlpExporter());
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHostedService<KafkaConsumerService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
