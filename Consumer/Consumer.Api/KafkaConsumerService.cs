b123e9e5a272bb7221713a9ae836e17b8746150d
J4XN_ <jackson.mourao@grupoboticario.com.br>
2024-07-15 17:51:47 -0300

    Added http request to .net app.

--- Consumer/Consumer.Api/KafkaConsumerService.cs
+++ Consumer/Consumer.Api/KafkaConsumerService.cs
@@ -1,5 +1,7 @@
 ﻿using Confluent.Kafka;
 using Confluent.Kafka.Extensions.Diagnostics;
+using System.Net.Http;
+using System.Text;
 
 namespace Consumer.Api
 {
@@ -7,6 +9,7 @@ namespace Consumer.Api
     {
         private readonly IConsumer<Ignore, string> _consumer;
         private readonly ILogger<KafkaConsumerService> _logger;
+        private readonly HttpClient _httpClient;
 
         public KafkaConsumerService(IConfiguration configuration, ILogger<KafkaConsumerService> logger)
         {
@@ -20,6 +23,7 @@ namespace Consumer.Api
             _consumer = new ConsumerBuilder<Ignore, string>(config).Build();
             _consumer.Subscribe("text-topic");
             _logger = logger;
+            _httpClient = new HttpClient();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -30,15 +34,24 @@ namespace Consumer.Api
             {
                 try
                 {
-                    await _consumer.ConsumeWithInstrumentation((result, cancelToken) =>
+                    await _consumer.ConsumeWithInstrumentation(async (result, cancelToken) =>
                     {
                         if (!cancelToken.IsCancellationRequested && result != null)
                         {
                             string text = result?.Message.Value ?? string.Empty;
                             _logger.LogInformation($"Consumed message '{text}' at: '{result?.TopicPartitionOffset}'.");
 
-                            // TODO: Send HTTP request to Golang App
+                            var content = new StringContent($"{{ \"text\": \"{text}\" }}", Encoding.UTF8, "application/json");
+                            var response = await _httpClient.PostAsync("http://localhost:8888/save-text", content, cancelToken);
 
+                            if (response.IsSuccessStatusCode)
+                            {
+                                _logger.LogInformation("HTTP POST request was successful.");
+                            }
+                            else
+                            {
+                                _logger.LogError($"HTTP POST request failed with status code {response.StatusCode}.");
+                            }
                         }
                         return Task.CompletedTask;
                     }, stoppingToken);
