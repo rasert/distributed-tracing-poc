ee6c6d0b17cd7ee14dbb2f4f0a348bdce810fe17
J4XN_ <jackson.mourao@grupoboticario.com.br>
2024-07-18 18:40:29 -0300

    Added std-out-trace

--- persistence/otel.go
+++ persistence/otel.go
@@ -6,13 +6,19 @@ import (
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+
+	// "go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
 )
 
 func SetupTelemetry(ctx context.Context) {
+	// stdout_exporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	otlp_exporter, err := otlptracehttp.New(ctx, otlptracehttp.WithEndpointURL("http://localhost:4318/v1/traces"))
 	if err != nil {
 		log.Fatal(err)
@@ -20,6 +26,7 @@ func SetupTelemetry(ctx context.Context) {
 
 	tp := trace.NewTracerProvider(
 		trace.WithBatcher(otlp_exporter),
+		// trace.WithBatcher(stdout_exporter),
 		trace.WithResource(resource.NewWithAttributes(
 			semconv.SchemaURL,
 			semconv.ServiceName("persistence-api"),
