74335f3d739e03b11565aeaadc099b3662a63070
J4XN_ <jackson.mourao@grupoboticario.com.br>
2024-07-19 15:02:00 -0300

    Added manual instrumentation to Node app.

--- publisher/app.js
+++ publisher/app.js
@@ -3,6 +3,9 @@ require('./tracing');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Kafka } = require('kafkajs');
+const opentelemetry = require('@opentelemetry/api');
+
+const tracer = opentelemetry.trace.getTracer('publisher', '1.0.0');
 
 const app = express();
 const port = parseInt(process.env.PORT || '8080');
@@ -15,16 +18,22 @@ app.post('/publish-text', async (req, res) => {
   const { text } = req.body;
   if (!text) {
     return res.status(400).send('Text is required');
-  }
+  }  
 
   try {
-    await producer.send({
-      topic: 'text-topic',
-      messages: [
-        { value: text }
-      ],
+    tracer.startActiveSpan('publish-text', async (span) => {
+      span.addEvent('publishing text to Kafka', { text });
+  
+      await producer.send({
+        topic: 'text-topic',
+        messages: [
+          { value: text }
+        ],
+      });
+      res.status(200).send('Message sent to Kafka');
+  
+      span.end();
     });
-    res.status(200).send('Message sent to Kafka');
   } catch (error) {
     console.error('Failed to send message:', error);
     res.status(500).send('Failed to send message');
